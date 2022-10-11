f2c1524c8385def5a31c01ba07105a18fbcd56aa
pieterkdevilliers <pieterkdevilliers@me.com>
2022-10-11 09:45:09 +0000

    Add check api_key evnt

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,5 +2,17 @@ const API_KEY = "ZQfuhcmjmX4Sh3sZpOE9ii5O1jw";
 const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
+document.getElementById("status").addEventListener("click", e => getStatus(e));
 
+async function getStatus(e) {
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        console.log(data);
+    }
+}
 
