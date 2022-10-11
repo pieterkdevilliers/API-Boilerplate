cf9f8fc2ec1f34926a7dafafb8640ce927ed837a
pieterkdevilliers <pieterkdevilliers@me.com>
2022-10-11 09:38:03 +0000

    Add js constants

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+const API_KEY = "ZQfuhcmjmX4Sh3sZpOE9ii5O1jw";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+
+
